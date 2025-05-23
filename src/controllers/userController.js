3407027716a8db0b721ba20a789f12cbf75a3753
Manas Baranwal <manas.baranwal@austere.co.in>
2025-05-24 01:07:45 +0530

    changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -104,7 +104,7 @@ exports.loginUser = async (req, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
-        phone: user.phone,
+        phone: user.phone || '',
         role: user.role,
         parent: user.parent,
         dependents: user.dependents.map((data) => ({
